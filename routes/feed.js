5bcb17cf355ec036c767b52e3c95f634e935fd97
sourabh agrawal <sourabhkhs23@gmail.com>
2022-12-17 00:07:06 +0530

    Feed GET and POST setup

--- routes/feed.js
+++ routes/feed.js
@@ -4,6 +4,10 @@ const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
+// GET /feed/posts
 router.get('/posts', feedController.getPosts);
+
+// POST /feed/posts
+router.post('/posts', feedController.createPost);
 
 module.exports = router;
