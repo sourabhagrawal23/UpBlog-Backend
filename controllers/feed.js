535d50165161e21197cd939be043398f0c4d4698
sourabh agrawal <sourabhkhs23@gmail.com>
2022-12-17 19:42:13 +0530

    Post storage

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator/check');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [
@@ -18,7 +20,7 @@ exports.getPosts = (req, res, next) => {
 
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()) {
+    if (!errors.isEmpty()) {
         return res.status(422).json({
             message: 'Validation failed, entered data is incorrect.',
             errors: errors.array()
@@ -27,15 +29,22 @@ exports.createPost = (req, res, next) => {
 
     const title = req.body.title;
     const content = req.body.content;
-    //Create post in db
-    res.status(201).json({
-        message: 'Post created successfully!',
-        post: { 
-            _id: new Date().toISOString, 
-            title: title, 
-            content: content,
-            creator: { name: 'Sourabh' },
-            createdAt: new Date()
-         }
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: 'images/img.png',
+        creator: { name: 'Sourabh' }
     });
+
+    post.save().then(result => {
+        //Create post in db
+        res.status(201).json({
+            message: 'Post created successfully!',
+            post: result
+        });
+    })
+        .catch(err => {
+            console.log(err);
+        })
+
 };
