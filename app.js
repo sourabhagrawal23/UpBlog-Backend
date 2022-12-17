ccff380b830a720c12acc4b7e6b304ea97fbaef7
sourabh agrawal <sourabhkhs23@gmail.com>
2022-12-17 13:51:35 +0530

    CORS Headers

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const bodyParser = require('body-parser');
 const feedRoutes = require('./routes/feed')
 
 const app = express();
-app.use(bodyParser.urlencoded()); //x-www-form-urlencoded <form>
+
+// app.use(bodyParser.urlencoded()); //x-www-form-urlencoded <form>
 app.use(bodyParser.json()); //application/json
 
-app.use('/feed',feedRoutes)
+app.use((res, res, next) => {
+    //CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/feed',feedRoutes);
 
 app.listen(8080);
-
