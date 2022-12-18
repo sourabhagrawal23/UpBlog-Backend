d82c722c49ea1fe513c4f87073e74ed8db31e820
sourabh agrawal <sourabhkhs23@gmail.com>
2022-12-18 18:49:49 +0530

    User SignUp and SignUp Validation

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const { Schema } = require('mongoose');
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        default: 'I am new!'
+    },
+    posts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Post'
+    }]
+});
+
+module.exports = mongoose.model('User', userSchema);
