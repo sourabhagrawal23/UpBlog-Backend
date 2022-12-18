13052679e8c34b0572d307bd6ff55b02935f59fa
sourabh agrawal <sourabhkhs23@gmail.com>
2022-12-18 15:27:48 +0530

    Delete functionality implemented

--- controllers/feed.js
+++ controllers/feed.js
@@ -147,6 +147,32 @@ exports.updatePost = (req, res, next) => {
     })
 };
 
+exports.deletePost = (req,res,next) => {
+    console.log('inside delete')
+    const postId = req.params.postId;
+    Post.findById(postId)
+    .then(post => {
+        if(!post) {
+            const error = new Error('Could not find post.');
+            error.statusCode = 404;
+            throw error;
+        }
+        //Check if creator is currently loggedIn user
+        clearImage(post.imageUrl);
+        return Post.findByIdAndRemove(postId);
+    })
+    .then(result => {
+        console.log(result);
+        res.status(200).json({message: 'Deleted Post'});
+    })
+    .catch(err => {
+        if(!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    })
+}
+
 const clearImage = filePath => {
     filePath = path.join(__dirname, '..', filePath);
     fs.unlink(filePath, err => console.log(err));
