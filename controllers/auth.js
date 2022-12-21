c5cd94bbdfc34c23c39009ba670f17ca032b35a8
sourabh agrawal <sourabhkhs23@gmail.com>
2022-12-21 23:55:03 +0530

    Token expiry modified

--- controllers/auth.js
+++ controllers/auth.js
@@ -60,7 +60,7 @@ exports.login = (req, res, next) => {
         const token = jwt.sign({
             email: loadedUser.email, 
             userId: loadedUser._id.toString()},
-            'somesupersecretsecret', {expiresIn: '1h'});
+            'somesupersecretsecret', {expiresIn: '5h'});
             res.status(200).json({token: token, userId: loadedUser._id.toString()});
     })
     .catch(err => {
