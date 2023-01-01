04cb8d985bceb6c5939133f24fa16c470e6b382e
sourabh agrawal <sourabhkhs23@gmail.com>
2023-01-01 20:29:28 +0530

    Sorting

--- controllers/feed.js
+++ controllers/feed.js
@@ -16,8 +16,9 @@ exports.getPosts = async (req, res, next) => {
         const totalItems = await Post.find().countDocuments();
         const posts = await Post.find()
         .populate('creator')
-            .skip((currentPage - 1) * perPage)
-            .limit(perPage);
+        .sort({createdAt: -1})
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
 
         res.status(200).json({
             message: 'Fetched posts successfully.',
